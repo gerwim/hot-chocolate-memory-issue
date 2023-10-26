49d94bcad53c5d8fe59644ac2cf8cf0b1b585a84
Pascal Senn <senn.pasc@gmail.com>
2023-10-26 21:23:06 +0200

    Use more efficent json serialization

--- HotChocolateMemoryIssue/Web/Program.cs
+++ HotChocolateMemoryIssue/Web/Program.cs
@@ -1,8 +1,18 @@
+using System.Text.Encodings.Web;
+using HotChocolate.AspNetCore.Serialization;
+using HotChocolate.Execution.Serialization;
 using Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHttpResponseFormatter(new HttpResponseFormatterOptions()
+{
+    Json = new JsonResultFormatterOptions()
+    {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    }
+});
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>();
